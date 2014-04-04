664eb3148a44c2b527797b8456c6b82c0532ca74
Eric-- <rmb5241@gmail.com>
2014-04-05 03:36:40 +0800

    ddd

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(req, ress){
-	
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.write("hello world");
-	res.end();
-}).listen(8888);
+function start(route, handle)
+{
+	function onRequest(req, res)
+	{
+		var postData = "";
+		var pathname = url.parse(req.url).pathname;
+		console.log("reque-st " + pathname + " received.");
+		route(handle, pathname, res, req);
+	};
+	http.createServer(onRequest).listen(8888);
+	console.log("server started");
+}
+
+exports.start = start;
