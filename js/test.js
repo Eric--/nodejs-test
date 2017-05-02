4287a0d1931203a13a0144cff823bfd0fd944a9c
erayliu <rmb5241@gmail.com>
2017-05-02 11:04:44 +0800

    modify ws

---
+++ js/test.js
@@ -0,0 +1,5 @@
+var Help = require('./help.js');
+
+var file = Help.root('src', 'tsconfig.json');
+
+console.log(process.env.NODE_ENV);
--- js/ws/server.js
+++ js/ws/server.js
@@ -2,13 +2,42 @@
  * Created by Administrator on 2017/3/19.
  */
 
-var WebSocketServer = require('ws').Server,
+var WebSocket = require('ws'),
+    WebSocketServer = WebSocket.Server,
     wss = new WebSocketServer({port: 8181});
 wss.on('connection', function (ws) {
     console.log('client connected');
+    var count = 0;
+    function sendData(){
+        var city = {
+            name: "sichuan",
+            city: "chengdu"
+        };
+        if ( count < 10) {
+            if(ws.readyState === WebSocket.OPEN) {
+                ws.send(JSON.stringify(city));
+            }
+            count++;
+        }else{
+            closeWs();
+        }
+    }
+    var intervalId = setInterval(sendData, 2000);
+
     ws.on('message', function (msg) {
-        console.log("server receive msg");
-        ws.send('server send: ' + msg);
+        console.log("server receive msg=" + msg);
+        if(msg && msg == 'close'){
+            closeWs();
+        }
     });
+
+    function closeWs(){
+        if(intervalId){
+            clearInterval(intervalId);
+        }
+        if(ws.readyState === WebSocket.OPEN){
+            ws.close();
+        }
+    }
 });
 
